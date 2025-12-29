c81e3e66e800871d3064b1692dc63b6ac9c3469d
Lukáš Koula <lukas.koula@gmail.com>
2025-12-29 13:48:14 +0100

    CORS: zjednoduší konfiguraci - CORS() na všech routech

--- formular/app.py
+++ formular/app.py
@@ -9,7 +9,7 @@ from flask import Flask, jsonify, redirect, request
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "*", "methods": ["GET", "POST", "OPTIONS"]}}, supports_credentials=True)
+CORS(app)
 
 
 class Config:
