28bb9b7b5326f79e4d0e397e35e5440ff3604c2e
Lukáš Koula <lukas.koula@gmail.com>
2025-12-29 13:42:17 +0100

    Formulář: přidat produkční backend URL a CORS support

--- formular/app.py
+++ formular/app.py
@@ -6,8 +6,10 @@ from typing import Dict
 
 import requests
 from flask import Flask, jsonify, redirect, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": ["https://skolniweby.cz", "https://www.skolniweby.cz"]}})
 
 
 class Config:
